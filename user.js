13460b2b7ebbf7fd1bc8246842bd1b8dae0c1c64
DanewellXalynx <141164215+DanewellXalynx@users.noreply.github.com>
2024-08-11 07:32:34 +0000

    Update user.js
    added more prefrences

--- user.js
+++ user.js
@@ -2,7 +2,8 @@
 
 //you are free to copy this file, commit to this file, redistribute it or fork it//
 
-//Disable telemetry collected by firefox//
+//Disable telemetry, studies, crash reports and similar//
+/*telemetry*/
 user_pref("devtools.onboarding.telemetry.logged", false);
 user_pref("toolkit.telemetry.updatePing.enabled", false);
 user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
@@ -10,6 +11,7 @@ user_pref("browser.newtabpage.activity-stream.telemetry", false);
 user_pref("browser.ping-centre.telemetry", false);
 user_pref("toolkit.telemetry.bhrPing.enabled", false);
 user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.server", "data:,");
 user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
 user_pref("toolkit.telemetry.hybridContent.enabled", false);
 user_pref("toolkit.telemetry.newProfilePing.enabled", false);
@@ -22,12 +24,54 @@ user_pref("toolkit.telemetry.archive.enabled", false);
 user_pref("devtools.onboarding.telemetry.logged", false);
 user_pref("datareporting.healthreport.uploadEnabled ", false);
 user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("toolkit.telemetry.coverage.opt-out", true)
+user_pref("toolkit.coverage.opt-out", true);
 user_pref("datareporting.sessions.current.clean ", true);
+user_pref("toolkit.coverage.endpoint.base", "");
+/*studies*/
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("app.normandy.enabled", false);
+user_pref("app.normandy.api_url", "");
+/*crash reports*/
+user_pref("breakpad.reportURL", "");
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
 
-//Disable Mozilla junk (work in progress)//
+//Disable Mozilla bloat//
+/*mozilla junk*/
 user_pref("extensions.pocket.enabled", false);
 user_pref("browser.preferences.moreFromMozilla", false);
 user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.activity-stream.default.sites", "");
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
 
-//Fingerprinting Protection (work in progress)//
+//Disable Recommendations//
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+user_pref("browser.discovery.enabled", false);
+user_pref("browser.shopping.experience2023.enabled", false);
+
+//Disable google code//
+/*Google Analytics*/
+user_pref("extensions.getAddons.showPane", false)
+/*Google Safe Browsing*/
+user_pref("browser.safebrowsing.malware.enabled", false);
+user_pref("browser.safebrowsing.phishing.enabled", false);
+user_pref("browser.safebrowsing.downloads.enabled", false);
+user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+user_pref("browser.safebrowsing.downloads.remote.url", "");
+user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
+user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false);
+
+//Tracking Protection//
 user_pref("privacy.resistFingerprinting", true);
+user_pref("webgl.disabled", true);
+user_pref("browser.contentblocking.category", "strict");
+
+//Clear data on shutdown//
+user_pref("privacy.clearOnShutdown.history", true);
+user_pref("privacy.clearOnShutdown.formdata", true);
+user_pref("privacy.clearOnShutdown.cache", true);
+user_pref("privacy.clearOnShutdown.cookies", true);
+user_pref("privacy.clearOnShutdown_v2.cache", true);
+user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", true);
+user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", true);
